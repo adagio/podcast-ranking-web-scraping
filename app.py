df6320427ce808a1ce5196bac056226e0cf95997
Franco Cedillo <franco.cedillo@gmail.com>
2018-11-13 22:56:54 -0500

    refactor url paths

--- app.py
+++ app.py
@@ -3,25 +3,32 @@ from pprint import pprint
 from modules.utils import *
 from modules.scraping import *
 
+path = 'sandbox/content/'
 
-filepaths = [
-    'sandbox/content/podcast_internet_tecnologia_1.html',
-    'sandbox/content/podcast_internet_tecnologia_2.html'
+filenames = [
+    'podcast_internet_tecnologia_1.html',
+    'podcast_internet_tecnologia_2.html'
 ]
 
-urls = [
-    'https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_1.html',
-    'https://www.ivoox.com/podcast-internet-tecnologia_sc_f445_2.html'
+protocol = 'https'
+domain = 'www.ivoox.com'
+base_path = protocol + '://' +  domain + '/'
+
+url_paths = [
+    'podcast-internet-tecnologia_sc_f445_1.html',
+    'podcast-internet-tecnologia_sc_f445_2.html'
 ]
 
 
 podcasts = []
 
-"""for filepath in filepaths:
+"""for filename in filenames:
+    filepath = path + filename
     soup = getSoup(read_file(filepath))
     get_podcasts(soup, podcasts)"""
 
-for url in urls:
+for url_path in url_paths:
+    url = base_path + url_path
     soup = getSoup(get_url_content(url))
     get_podcasts(soup, podcasts)
 
