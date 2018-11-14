9b5086391b13691fc029d7542453fec6361499cc
Franco Cedillo <franco.cedillo@gmail.com>
2018-11-14 01:12:54 -0500

    try populate data class from json

--- sandbox/data_classes.py
+++ sandbox/data_classes.py
@@ -1,15 +1,28 @@
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+import json
 
-@dataclass
+value = {
+    'title': 'tu',
+    'desc': 'yes'
+}
+value_json = json.dumps(value)
+
+@dataclass_json
+@dataclass(frozen=True)
 class DataClassPodcast:
     title: str
     desc: str
     episode: str
     url: str
+    
+"""     def __init__(self, value):
+        self.title = value['title']
+        self.desc = value['desc']
+        self.episode = '777'
+        self.url = 'uu' """
 
 #podcast = DataClassPodcast('tt', 'dd', 777, 'uu')
-podcast = DataClassPodcast()
-
-podcast.title = 'hola'
+podcast = DataClassPodcast.from_json(value_json)
 
 print(podcast)
